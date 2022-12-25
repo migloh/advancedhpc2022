e726b30676c529d2774f4653ae0dc82b168c8d0c
Ming Lonh <minhlong.wb@gmail.com>
2022-12-25 10:59:04 +0700

    [Labwork 5] GPU sans sharing

--- Labwork05/Labwork5.py
+++ Labwork05/Labwork5.py
@@ -2,12 +2,9 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import cuda
-from PIL import Image
 import cv2
 
 img = plt.imread('images/bird.png')
-img_h = img.shape[0]
-img_w = img.shape[1]
 
 gauss = np.array([
     [0, 0, 1, 2, 1, 0, 0],
@@ -22,6 +19,7 @@ gauss_sum = np.sum(gauss)
 
 img_pad = cv2.copyMakeBorder(
     img, 3, 3, 3, 3, cv2.BORDER_CONSTANT, None, value=0)
+pad_h, pad_w, _ = img_pad.shape
 print(img.dtype)
 print(img_pad.dtype)
 
@@ -47,49 +45,44 @@ plt.imsave('images/cpu2d.png', blurimg)
 
 print("Manual transformation time: ", stop_man-start_man)
 
-# grayimg = np.array(img, copy=True)
-# devOutput = cuda.device_array(img.shape, img.dtype)
+blurimg = np.array(img, copy=True)
+devOutput = cuda.device_array(img.shape, img.dtype)
 
-# devInput = cuda.to_device(img_pad)
-
-
-# @cuda.jit
-# def grayscale(src, dst):
-#     tidx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
-#     tidy = cuda.threadIdx.y + cuda.blockIdx.y * cuda.blockDim.y
-#     gauss = np.array([
-#         [0, 0, 1, 2, 1, 0, 0],
-#         [0, 3, 13, 22, 13, 3, 0],
-#         [1, 13, 59, 97, 59, 13, 1],
-#         [2, 22, 97, 159, 97, 22, 2],
-#         [1, 13, 59, 97, 59, 13, 1],
-#         [0, 3, 13, 22, 13, 3, 0],
-#         [0, 0, 1, 2, 1, 0, 0],
-#     ])
-
-#     rr = 0
-#     gg = 0
-#     bb = 0
-
-#     for i in gauss.shape[0]:
-#         for j in gauss.shape[1]:
-#             if tidx < 3 or tidx > src.shape[0]- 3 or tidy < 3 or tidy > src.shape[1]-3
-#             g = (src[tidx, tidy, 0] + src[tidx, tidy, 1] + src[tidx, tidy, 2])/3
-
-#     dst[tidx, tidy, 0] = dst[tidx, tidy, 1] = dst[tidx, tidy, 2] = g
+devInput = cuda.to_device(img_pad)
 
 
-# blSz = 32
-# gridSize = (np.uint(img_h/blSz), np.uint(img_w/blSz))
-# blockSize = (blSz, blSz)
+@cuda.jit
+def grayscale(src, dst, filter):
+    tidx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
+    tidy = cuda.threadIdx.y + cuda.blockIdx.y * cuda.blockDim.y
 
-# start_cuda = time.time()
-# grayscale[gridSize, blockSize](devInput, devOutput)
-# stop_cuda = time.time()
+    if tidx < 3 or tidx > src.shape[0]-3 or tidy < 3 or tidy > src.shape[1] - 3:
+        return
 
-# hostOutput = devOutput.copy_to_host()
-# plt.imsave('images/gpu2d.png', hostOutput)
-# print('Cuda transformation time: ', stop_cuda-start_cuda)
+    rr = 0
+    gg = 0
+    bb = 0
+    for ii in range(0, 7):
+        for jj in range(0, 7):
+            rr += src[tidx-3+ii, tidy-3+jj, 0]*filter[ii, jj]
+            gg += src[tidx-3+ii, tidy-3+jj, 1]*filter[ii, jj]
+            bb += src[tidx-3+ii, tidy-3+jj, 2]*filter[ii, jj]
+    dst[tidx-3, tidy-3, 0] = np.float32(rr/1003)
+    dst[tidx-3, tidy-3, 1] = np.float32(gg/1003)
+    dst[tidx-3, tidy-3, 2] = np.float32(bb/1003)
+
+
+blSz = 32
+gridSize = (np.uint(pad_h/blSz), np.uint(pad_w/blSz))
+blockSize = (blSz, blSz)
+
+start_cuda = time.time()
+grayscale[gridSize, blockSize](devInput, devOutput, gauss)
+stop_cuda = time.time()
+
+hostOutput = devOutput.copy_to_host()
+plt.imsave('images/gpu2d.png', hostOutput)
+print('Cuda transformation time: ', stop_cuda-start_cuda)
 
 # block_size = [2, 4, 8, 16, 32]
 # collapsed_time = []
